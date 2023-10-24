a123c9c29176115ab6f6e315d3c6836063cf170b
Sardor <sardoribodov07@gmail.com>
2023-10-24 08:53:51 +0500

    Lessons are uploaded

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 // import Lesson2 from "./2-lesson/wrapper";
 // import Lesson3 from "./3-lesson";
-// import Prepare from "./prepare";
 import Lesson4 from "./4-lesson";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <>
     {/* <Lesson2 /> */}
 		{/* <Lesson3 /> */}
-		{/* <Prepare /> */}
 		<Lesson4 />
   </>
 );
